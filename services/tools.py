190c0e12688e8d749f10d94d3adb54d4e79f61bc
Alexander Beley <beleysasha@gmail.com>
2026-03-20 16:29:26 +1200

    some fixes in url validation

--- services/tools.py
+++ services/tools.py
@@ -6,6 +6,7 @@ from pprint import pprint
 from datetime import datetime
 
 from yt_dlp import YoutubeDL
+from youtube_urls_validator import validate_url
 
 # To update yt_dlp execute pip install -U yt-dlp
 # If 404 error
